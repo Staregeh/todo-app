378c5724302e45f6770988efc79d78f0bba838d4
staregeh <kushkaoleg@gmail.com>
2023-03-09 11:43:24 +0200

    bugs

--- client/src/modules/common/components/Authentication/Auth.tsx
+++ client/src/modules/common/components/Authentication/Auth.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Box, Typography, ThemeProvider, Button } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import theme from "../../consts/theme";
 import { SPACES } from "../../../theme";
 
